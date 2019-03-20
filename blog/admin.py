6b37baac066559dee50d59250238ec57c884a011
AlexBlueCrow <forstarcraft213@163.com>
2019-03-20 10:31:46 +0800

    first commit

---
+++ blog/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Post
+
+admin.site.register(Post)
+# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title','slug','author','publish','status')
